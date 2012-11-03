7a319070d101139c8d595e4d396ee14b4e514a91
mattray <matthewhray@gmail.com>
2012-11-03 00:40:26 -0500

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# CHANGELOG for ipxe
+
+## 0.1.0:
+
+* Initial release of ipxe
+* build and chainload recipes
+
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer        "Opscode, Inc."
+maintainer_email  "matt@opscode.com"
+license           "Apache 2.0"
+description       "Installs/Configures ipxe"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.1.0"
+
+depends "build-essential"
+depends "git"
